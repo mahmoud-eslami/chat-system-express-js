c2ecdf45ddf9d9997b8cfb80f7d70544a843740f
mahmoud <eslamimahmoud98@gmail.com>
2021-07-11 15:10:54 +0430

    new change

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// new way to jwt token
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const app = express()
+
+const { login, refresh } = require('./authentication')
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.post('/login', login)
+app.post('/refrsh', refresh)
