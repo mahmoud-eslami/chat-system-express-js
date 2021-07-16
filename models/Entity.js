37be7308196e7b62504f38fb79b117082540c9ab
mahmoud <eslamimahmoud98@gmail.com>
2021-07-16 22:32:49 +0430

    fkey added

--- models/Entity.js
+++ models/Entity.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../database');
 const group = require('../models/Group');
+const channel = require('../models/Channel');
+const user = require('../models/User');
 
 
 const Entity = sequelize.define('Entity', {
@@ -12,5 +14,9 @@ const Entity = sequelize.define('Entity', {
 }, {
     timestamps: false,
 });
+
+Entity.belongsTo(channel, { foreignKey: { name: 'cid', allowNull: true } });
+Entity.belongsTo(group, { foreignKey: { name: 'gid', allowNull: true } });
+Entity.belongsTo(user, { foreignKey: { name: 'uid', allowNull: true } });
 
 module.exports = Entity;
