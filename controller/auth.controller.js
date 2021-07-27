898c9782ffaf5c8fcdfdd7e9f43c45150648e312
mahmoud <eslamimahmoud98@gmail.com>
2021-07-27 12:11:05 +0430

    login completed token retrieve

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,13 +1,50 @@
 const user = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config.json');
 
-exports.login = (req, res) => {
-    res.send('login');
+exports.login = async(req, res) => {
+    try {
+        // todo : unhash password for check
+        const temp_user = await user.findOne({
+            where: {
+                username: req.body.username,
+                password: req.body.password,
+            }
+        });
+
+        if (temp_user === null) {
+            res.status(404).json({
+                error: true,
+                message: "Wrong username or password!",
+            });
+        } else {
+            const token = jwt.sign({
+                userId: temp_user.userId,
+                username: temp_user.username
+            }, config.secret, {
+                expiresIn: config.tokenLife,
+            });
+            res.status(200).json({
+                error: false,
+                message: {
+                    token: token,
+                }
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: true,
+            message: e
+        });
+    }
 };
 exports.refreshToken = (req, res) => {
     res.send('refresh');
 };
 exports.register = async(req, res) => {
     try {
+        // todo : hash password
         const temp_user = await user.findOne({
             where: {
                 username: req.body.username,
@@ -15,7 +52,10 @@ exports.register = async(req, res) => {
         });
         console.log(temp_user);
         if (temp_user !== null) {
-            res.json({ "error": true, "message": "username duplicated!" });
+            res.status(409).json({
+                error: true,
+                message: "user already exist!"
+            });
         } else {
             await user.create({
                 name: req.body.name,
@@ -23,12 +63,18 @@ exports.register = async(req, res) => {
                 password: req.body.password,
             });
 
-            res.json({ "error": false, "message": "user created!" });
+            res.status(200).json({
+                error: false,
+                message: "user created!"
+            });
         }
 
-    } catch (error) {
-        console.log(error);
-        res.json({ "error": true, "message": error });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: true,
+            message: e
+        });
     }
 
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const auth = require('../controller/auth.controller');
 const tokenChecker = require('../middleware/tokenChecker')
 
 module.exports = app => {
-    app.get('/login', tokenChecker, auth.login);
+    app.get('/login', auth.login);
     app.get('/refreshToken', tokenChecker, auth.refreshToken);
     app.get('/register', auth.register);
 };
