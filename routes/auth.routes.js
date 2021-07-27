da651aa68c64497334a823a9cced922474750d54
mahmoud <eslamimahmoud98@gmail.com>
2021-07-27 09:24:05 +0430

    token checker as middleware added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 const auth = require('../controller/auth.controller');
+const tokenChecker = require('../middleware/tokenChecker')
 
 module.exports = app => {
-    app.get('/login', auth.login);
-    app.get('/refreshToken', auth.refreshToken);
-    app.get('/register', auth.register);
+    app.get('/login', tokenChecker, auth.login);
+    app.get('/refreshToken', tokenChecker, auth.refreshToken);
+    app.get('/register', tokenChecker, auth.register);
 };
