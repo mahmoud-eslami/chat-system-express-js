fb8ea650a28807e6605f4887914dc589c3a3b781
mahmoud <eslamimahmoud98@gmail.com>
2021-07-28 12:50:45 +0430

    email field added to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,29 +1,41 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../config/database.config');
-const bcrypt = require('bcrypt');
+const Sequelize = require("sequelize");
+const sequelize = require("../config/database.config");
+const bcrypt = require("bcrypt");
 
-const User = sequelize.define('User', {
-    userId: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    username: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
+const User = sequelize.define(
+    "User", {
+        userId: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: "Must be a valid email address",
+                },
+            },
+        },
+
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    }, {
+        freezeTableName: true,
+        updatedAt: false,
     }
-}, {
-    freezeTableName: true,
-    updatedAt: false,
-
-});
+);
 
 module.exports = User;
